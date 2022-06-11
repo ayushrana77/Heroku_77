9c2a42aa8aa6587d0f42b8e0dfd2d3f2990eda4e
Ayush rana <ayushrana200177@gmail.com>
2022-06-11 12:32:18 +0530

    add antoner folder put app. py

--- ins/app.py
+++ ins/app.py
@@ -1,7 +1,8 @@
 from flask import Flask,send_from_directory
 from flask_cors import CORS,cross_origin
 
-app = Flask(__name__,static_folder='my-app/build',static_url_path='')
+
+app = Flask(__name__,static_folder='../my-app/build',static_url_path='')
 CORS(app)
 @app.route('/api', methods=['GET'])
 @cross_origin()
